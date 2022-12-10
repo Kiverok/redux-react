cce2383f9b12429ecd7f0b978b4dfe9f97e78cbe
Kiverok <kiver566@ukr.net>
2022-12-10 12:32:12 +0200

    newPost

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -5,11 +5,26 @@ export default class PostForm extends React.Component {
     constructor(props) {
         super(props)
 
-        this.state = {}
+        this.state = {
+            title: ''
+        }
     }
 
     submitHandler = event => {
         event.preventDefault()
+
+        const {title} = this.state
+
+        const newPost = {
+title, id: Date.now().toString()
+        }
+    }
+
+    changeInputHandler = event => {
+        event.persist()
+        this.setState( prev => ({...prev, ...{
+            [event.target.name]: event.target.value
+        }}))
     }
 
     render() {
@@ -17,7 +32,14 @@ export default class PostForm extends React.Component {
         <form onSubmit={this.submitHandler}>
             <div className="form-group">
     <label htmlfor="title">Заголовок</label>
-    <input type="text" className="form-control" id="title" />
+    <input 
+    type="text" 
+    className="form-control" 
+    id="title" 
+    value={this.state.title}
+    name="title"
+    onChange={this.changeInputHandler}
+    />
   </div>
   <button className="btn btn-success" type="submit">Створити</button>
         </form>
