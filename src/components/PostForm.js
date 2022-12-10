aabecaf2c23fe1a3165b0e4708433c806520f4fe
Kiverok <kiver566@ukr.net>
2022-12-10 12:05:23 +0200

    componentsForm

---
+++ src/components/PostForm.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default class PostForm extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {}
+    }
+
+    submitHandler = event => {
+        event.preventDefault()
+    }
+
+    render() {
+        return ( 
+        <form onSubmit={this.submitHandler}>
+            <div className="form-group">
+    <label htmlfor="title">Заголовок</label>
+    <input type="text" className="form-control" id="title" />
+  </div>
+  <button className="btn btn-success" type="submit">Створити</button>
+        </form>
+        )
+    }
+}
